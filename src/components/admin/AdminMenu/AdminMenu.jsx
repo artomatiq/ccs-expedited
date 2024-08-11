01592bbfb594624ca228f375f484fa1a3afc74e3
Art Vardanyan <akvardanyan99@gmail.com>
2024-08-10 20:17:09 -0400

    logout button

--- src/components/admin/AdminMenu/AdminMenu.jsx
+++ src/components/admin/AdminMenu/AdminMenu.jsx
@@ -24,6 +24,27 @@ function AdminMenu() {
                 </a>
                 <span className="menu-hover">Reports</span>
             </li>
+            <li className="menu-item">
+                <a href='#1'>
+                    <i class='bx bx-time-five menu-icon'></i>
+                    <span className="menu-name">Rates</span>
+                </a>
+                <span className="menu-hover">Rates</span>
+            </li>
+            <li className="menu-item">
+                <a href='#1'>
+                    <i class='bx bxs-cog menu-icon' ></i>
+                    <span className="menu-name">Settings</span>
+                </a>
+                <span className="menu-hover">Settings</span>
+            </li>
+            <li className="menu-item logout">
+                <a href='#1'>
+                <i class='bx bx-log-out menu-icon' ></i>
+                    <span className="menu-name">Logout</span>
+                </a>
+                <span className="menu-hover">Logout</span>
+            </li>
         </ul>
     );
 }
