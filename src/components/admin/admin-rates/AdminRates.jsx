3f219b47125d251376c9da262edab39b5289d3ba
Art Vardanyan <akvardanyan99@gmail.com>
2024-08-11 18:13:15 -0400

    AdminRates component

---
+++ src/components/admin/admin-rates/AdminRates.jsx
@@ -0,0 +1,11 @@
+import './AdminRates.css';
+
+function AdminRates() {
+    return (
+        <div className="admin rates-container">
+            Admin Rates
+        </div>
+    );
+}
+
+export default AdminRates;
--- src/components/main-content/MainContent.jsx
+++ src/components/main-content/MainContent.jsx
@@ -2,6 +2,7 @@ import './MainContent.css';
 
 import AdminDashboard from '../admin/admin-dashboard/AdminDashboard';
 import AdminPayroll from '../admin/admin-payroll/AdminPayroll';
+import AdminRates from '../admin/admin-rates/AdminRates';
 import AdminReports from '../admin/admin-reports/AdminReports';
 import AdminSettings from '../admin/admin-settings/AdminSettings';
 
@@ -20,8 +21,10 @@ function MainContent() {
     return (
 
         <Routes>
-            <Route path='/' element={role === 'admin' ? <AdminDashboard/> : <DriverDashboard/>} />
+            <Route path='/' element={role === 'admin' ? <AdminDashboard/> : <DriverClock/>} />
+            <Route path='dashboard' element={role === 'admin' ? <AdminDashboard/> : <DriverDashboard/>} />
             <Route path='payroll' element={<AdminPayroll />} />
+            <Route path='rates' element={<AdminRates />} />
             <Route path='earnings' element={<DriverEarnings />} />
             <Route path='clock' element={<DriverClock />} />
             <Route path='reports' element={role === 'admin' ? <AdminReports/> : <DriverReports/>} />
