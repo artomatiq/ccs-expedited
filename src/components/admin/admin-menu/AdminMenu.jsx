3a824c75d857e63b42eb3122d060a5456639aad0
Art Vardanyan <akvardanyan99@gmail.com>
2024-08-11 18:09:54 -0400

    driver menu

--- src/components/admin/admin-menu/AdminMenu.jsx
+++ src/components/admin/admin-menu/AdminMenu.jsx
@@ -4,50 +4,38 @@ import './AdminMenu.css';
 
 function AdminMenu() {
 
-    
+
 
     return (
         <ul className="admin menu-container">
             <li className="menu-item">
-                <a href='#1'>
-                    <i className='bx bx-tachometer menu-icon'></i>
-                    <span className="menu-name">Dashboard</span>
-                </a>
+                <i className='bx bx-tachometer menu-icon'></i>
+                <span className="menu-name">Dashboard</span>
                 <span className="menu-hover">Dashboard</span>
             </li>
             <li className="menu-item">
-                <a href='#1'>
-                    <i className='bx bx-money-withdraw menu-icon'></i>
-                    <span className="menu-name">Payroll</span>
-                </a>
+                <i className='bx bx-money-withdraw menu-icon'></i>
+                <span className="menu-name">Payroll</span>
                 <span className="menu-hover">Payroll</span>
             </li>
             <li className="menu-item">
-                <a href='#1'>
-                    <i className='bx bxs-report menu-icon'></i>
-                    <span className="menu-name">Reports</span>
-                </a>
+                <i className='bx bxs-report menu-icon'></i>
+                <span className="menu-name">Reports</span>
                 <span className="menu-hover">Reports</span>
             </li>
             <li className="menu-item">
-                <a href='#1'>
-                    <i class='bx bx-time-five menu-icon'></i>
-                    <span className="menu-name">Rates</span>
-                </a>
+                <i class='bx bx-time-five menu-icon'></i>
+                <span className="menu-name">Rates</span>
                 <span className="menu-hover">Rates</span>
             </li>
             <li className="menu-item">
-                <a href='#1'>
-                    <i class='bx bxs-cog menu-icon' ></i>
-                    <span className="menu-name">Settings</span>
-                </a>
+                <i class='bx bxs-cog menu-icon' ></i>
+                <span className="menu-name">Settings</span>
                 <span className="menu-hover">Settings</span>
             </li>
             <li className="menu-item logout">
-                <a href='#1'>
                 <i class='bx bx-log-out menu-icon' ></i>
-                    <span className="menu-name">Logout</span>
-                </a>
+                <span className="menu-name">Logout</span>
                 <span className="menu-hover">Logout</span>
             </li>
         </ul>
--- src/components/driver/driver-menu/DriverMenu.jsx
+++ src/components/driver/driver-menu/DriverMenu.jsx
@@ -4,45 +4,33 @@ function DriverMenu() {
     return (
         <ul className="driver menu-container">
             <li className="menu-item">
-                <a href='#1'>
-                    <i className='bx bx-tachometer menu-icon'></i>
-                    <span className="menu-name">Clock</span>
-                </a>
+                <i className='bx bx-tachometer menu-icon'></i>
+                <span className="menu-name">Clock</span>
                 <span className="menu-hover">Clock</span>
             </li>
             <li className="menu-item">
-                <a href='#1'>
-                    <i className='bx bx-money-withdraw menu-icon'></i>
-                    <span className="menu-name">Dashboard</span>
-                </a>
+                <i className='bx bx-money-withdraw menu-icon'></i>
+                <span className="menu-name">Dashboard</span>
                 <span className="menu-hover">Dashboard</span>
             </li>
             <li className="menu-item">
-                <a href='#1'>
-                    <i className='bx bxs-report menu-icon'></i>
-                    <span className="menu-name">Earnings</span>
-                </a>
+                <i className='bx bxs-report menu-icon'></i>
+                <span className="menu-name">Earnings</span>
                 <span className="menu-hover">Earnings</span>
             </li>
             <li className="menu-item">
-                <a href='#1'>
-                    <i class='bx bx-time-five menu-icon'></i>
-                    <span className="menu-name">Reports</span>
-                </a>
+                <i class='bx bx-time-five menu-icon'></i>
+                <span className="menu-name">Reports</span>
                 <span className="menu-hover">Reports</span>
             </li>
             <li className="menu-item">
-                <a href='#1'>
-                    <i class='bx bxs-cog menu-icon' ></i>
-                    <span className="menu-name">Settings</span>
-                </a>
+                <i class='bx bxs-cog menu-icon' ></i>
+                <span className="menu-name">Settings</span>
                 <span className="menu-hover">Settings</span>
             </li>
             <li className="menu-item logout">
-                <a href='#1'>
                 <i class='bx bx-log-out menu-icon' ></i>
-                    <span className="menu-name">Logout</span>
-                </a>
+                <span className="menu-name">Logout</span>
                 <span className="menu-hover">Logout</span>
             </li>
         </ul>
