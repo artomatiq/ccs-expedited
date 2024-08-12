10d3a228b6eb5dbdbd875153d04e2cd0796a6e38
Art Vardanyan <akvardanyan99@gmail.com>
2024-08-12 13:58:37 -0400

    footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,6 +3,8 @@ import './Footer.css';
 function Footer() {
     return (
         <div className="footer-container">
+            <br/>
+            © Carolina's Courier Services. <br/> <br/>
             All rights reserved.
         </div>
     );
