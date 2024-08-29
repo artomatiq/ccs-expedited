77d3b1e86104fc8ff824881791458090c6bd6422
Art Vardanyan <akvardanyan99@gmail.com>
2024-08-29 19:16:20 -0400

    wip

--- server/index.js
+++ server/index.js
@@ -5,5 +5,5 @@ const server = require('./api/server.js');
 const port = process.env.PORT || 9000;
 
 server.listen(port, () => {
-    console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+    console.log(`server running on port ${port}`);
 })
