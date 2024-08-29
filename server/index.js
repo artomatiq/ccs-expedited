c8203597bc08ac2d1747c5cced61db3aecdf1ba2
Art Vardanyan <akvardanyan99@gmail.com>
2024-08-29 19:37:38 -0400

    wip

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const server = require('./api/server.js');
 
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
     console.log(`server running on port ${port}`);
