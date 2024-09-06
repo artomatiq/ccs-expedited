26740a045fd077e40c8c3af0f7db6276d423bea4
Art Vardanyan <akvardanyan99@gmail.com>
2024-09-06 14:19:37 -0400

    migrations

---
+++ server/data/migrations/20240906164531-create-drivers.js
@@ -0,0 +1,67 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+
+/*
+
+CREATE TABLE drivers (
+	id INT PRIMARY KEY,
+	name VARCHAR(100) NOT NULL,
+	email VARCHAR (100) NOT NULL,
+    phone VARCHAR (20) NOT NULL,
+    hourly_rate DECIMAL (10, 2) NOT NULL,
+    profile_picture VARCHAR(255)
+);
+
+*/
+
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('drivers', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      phone: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      hourly_rate: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+      },
+      profile_picture: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+        onUpdate: Sequelize.NOW
+      }
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('drivers');
+  }
+};
---
+++ server/data/migrations/20240906164825-create-timesheet.js
@@ -0,0 +1,65 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+
+/*
+
+CREATE TABLE timesheet (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    driver_id INT NOT NULL,
+    clock_in TIMESTAMP,
+    clock_pause TIMESTAMP,
+    clock_resume TIMESTAMP,
+    clock_out TIMESTAMP,
+    date DATE NOT NULL,
+    FOREIGN KEY (driver_id) REFERENCES drivers(id)
+);
+
+*/
+
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('timesheet', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      driver_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'drivers',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+      },
+      clock_in: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      clock_pause: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      clock_resume: {
+        type: Sequelize.DATE,
+        allowNull: true
+      },
+      clock_out: {
+        type: Sequelize.DATE,
+        allowNull: true
+      },
+      date: {
+        type: Sequelize.DATEONLY,
+        allowNull: false,
+      }
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('timesheet');
+  }
+};
