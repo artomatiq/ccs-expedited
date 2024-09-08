e98cb279cdd1238c3717c73dcf369fe02c42781b
Art Vardanyan <akvardanyan99@gmail.com>
2024-09-08 11:41:33 -0400

    timesheet model

---
+++ server/api/models/Timesheet.js
@@ -0,0 +1,55 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+    const Timesheet = sequelize.define('Timesheet', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        driver_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'drivers',
+                key: 'id'
+            },
+            onUpdate: 'CASCADE',
+            onDelete: 'CASCADE'
+        },
+        clock_in: {
+            type: DataTypes.DATE,
+            allowNull: true
+        },
+        clock_pause: {
+            type: DataTypes.DATE,
+            allowNull: true
+        },
+        clock_resume: {
+            type: DataTypes.DATE,
+            allowNull: true
+        },
+        clock_out: {
+            type: DataTypes.DATE,
+            allowNull: true
+        },
+        date: {
+            type: DataTypes.DATEONLY,
+            allowNull: false
+        }
+    }, {
+            timestamps: false,
+            tableName: 'timesheet'
+        }
+    );
+
+    Timesheet.associate = (models) => {
+        Timesheet.belongsTo(models.Driver, {
+            foreignKey: 'driver_id',
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE'
+        });
+    }
+
+    return Timesheet;
+}
