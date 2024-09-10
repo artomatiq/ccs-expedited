5e877089df320417105e576c43b7734c9bc3f054
Art Vardanyan <akvardanyan99@gmail.com>
2024-09-10 16:37:58 -0400

    wip

---
+++ server/utils/timesheetGenerator.js
@@ -0,0 +1,22 @@
+let entries = []
+
+let entry = {
+    driver_id: 1,
+    date: new Date(),
+    clock_in: new Date(),
+    clock_pause: null,
+    clock_resume: null,
+    clock_out: null,
+}
+
+// const time = new Date().toLocaleTimeString('en-GB')
+// 14:30:00 PM
+
+
+//pick random driver
+//
+
+//startDate
+function generateEntries (quantity) {
+    
+}
