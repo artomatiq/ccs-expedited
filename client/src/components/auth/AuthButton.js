08bc131615a1831ac00f8efa8fe6c0cc74d8b18f
Art Vardanyan <akvardanyan99@gmail.com>
2024-10-24 15:19:36 -0400

    AuthButton.js

---
+++ client/src/components/auth/AuthButton.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
+function AuthButton() {
+    const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
+
+    return isAuthenticated 
+    ? (
+        <button onClick={() => logout({returnTo: window.location.origin})}>
+            Logout
+        </button>
+    )
+    : (
+        <button onClick={loginWithRedirect}>
+            Login
+        </button>
+    )
+}
+
+export default AuthButton;
