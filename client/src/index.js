a7266b5fbe3cfe09fa8f0cfaeb0bbe1a6cfc0855
Art Vardanyan <akvardanyan99@gmail.com>
2024-10-24 13:43:00 -0400

    Auth0Provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter, useNavigate } from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
 
-import { BrowserRouter } from 'react-router-dom';
+const Auth0ProviderWrapper = ({ children }) => {
 
+  const navigate = useNavigate();
+
+  const onRedirectCallback = (appState) => {
+    navigate(appState?.returnTo || '/');
+  };
+
+  return (
+    <Auth0Provider
+      domain='dev-gc0cdlhjk7beuxzr.us.auth0.com'
+      clientId='agRbFwLVzeMmQW2ZsuJWcvypttPz9kGe'
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+      onRedirectCallback={onRedirectCallback}
+    >
+      {children}
+    </Auth0Provider>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+
   <BrowserRouter>
     <React.StrictMode>
+      <Auth0ProviderWrapper>
+        <App />
+      </Auth0ProviderWrapper>
       <App />
     </React.StrictMode>
   </BrowserRouter>
